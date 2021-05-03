6a92b877ec5cde616224107e6fb44a45520a00e8
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-05-03 15:34:29 -0300

    feat: hello world with database

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import "dotenv";
 import express from "express";
+import "./database";
+import { getCustomRepository } from "typeorm";
+import { AnnualBillingRepository } from "./repositories/AnnualBillingRepository";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+app.get("/", async (req, res) => {
+  const annualBillingsRepository = getCustomRepository(AnnualBillingRepository);
+  const annualBillings = await annualBillingsRepository.find();
+  return res.json(annualBillings);
 })
 
 const PORT = process.env.PORT || 3333;
