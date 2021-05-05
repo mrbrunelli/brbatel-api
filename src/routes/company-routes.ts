414b427a185196fe66d9ae837338a87f49fa7ef1
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-05-05 20:23:10 -0300

    refactor: implement celebrate middleware

--- src/routes/company-routes.ts
+++ src/routes/company-routes.ts
@@ -1,8 +1,9 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { CompanyController } from "../controllers/CompanyController";
+import { validateCompanyQuery } from "../middlewares/validator";
 
 export const companyRoutes = Router();
 
 const companyController = new CompanyController();
-companyRoutes.get("/", companyController.findAll);
+companyRoutes.get("/", validateCompanyQuery, companyController.findAll);
 companyRoutes.post("/", companyController.create);
