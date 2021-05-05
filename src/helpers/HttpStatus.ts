9e893419a151735bd545f8d07e0739e3d12d9421
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-05-04 22:01:32 -0300

    feat: add http status helper

---
+++ src/helpers/HttpStatus.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+
+export class HttpStatus {
+  static ok(res: Response, data: any) {
+    return res.json(data);
+  }
+
+  static badRequest(res: Response, message: string) {
+    return res.status(400).json(message);
+  }
+
+  static unauthorized(res: Response, message: string) {
+    return res.status(401).json(message);
+  }
+
+  static serverError(res: Response) {
+    return res.status(500).json("Ocorreu um erro inesperado no servidor.");
+  }
+}
