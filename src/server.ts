c3369ce5e4373faef656d6a14e3f83894989584d
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-05-05 20:21:01 -0300

    feat: add celebrate error handler

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import "dotenv";
 import express from "express";
 import "./database";
 import { routes } from "./routes";
+import { errors } from "celebrate";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log("Server running on http://localhost:" + PORT));
