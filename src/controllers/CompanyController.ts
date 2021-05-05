23ba456ba070ad77c265b63d8efb02eff3d20722
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-05-04 22:03:12 -0300

    feat: add CompanyController

---
+++ src/controllers/CompanyController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { HttpStatus } from "../helpers/HttpStatus";
+import { CompanyService } from "../services/CompanyService";
+
+export class CompanyController {
+  async findAll(req: Request, res: Response) {
+    try {
+      const companyService = new CompanyService();
+      const company = await companyService.findAll(req.query);
+      return HttpStatus.ok(res, company);
+    } catch (e) {
+      return HttpStatus.serverError(res);
+    }
+  }
+
+  async create(req: Request, res: Response) {
+    try {
+      const companyService = new CompanyService();
+      const company = await companyService.create(req.body);
+      return HttpStatus.ok(res, company);
+    } catch (e) {
+      return HttpStatus.serverError(res);
+    }
+  }
+}
