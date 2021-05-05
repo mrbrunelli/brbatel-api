e344554bb2616b4fc0fd547b5195081f157c0d15
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-05-05 20:22:56 -0300

    refactor: change to celebrate midleware

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -1,27 +1,19 @@
-import { NextFunction, Request, Response } from "express";
-import { validationResult, query } from "express-validator";
-import { HttpStatus } from "../helpers/HttpStatus";
+import { celebrate, Joi, Segments } from "celebrate";
 
-export const validate = async (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return HttpStatus.unprocessable(res, { errors: errors.array() });
-  }
-  return next();
-}
-
-export const companyQueryValidation = [
-  query("demand")
-    .optional()
-    .matches(/^\d+\,\s*?\d+/g)
-    .withMessage("Deve ser informado apenas dois valores, e separados por ','"),
-  query("created_at")
-    .optional()
-    .matches(/^(\d{4})-(\d{02})-(\d{02})\,\s?(\d{4})-(\d{2})-(\d{2})$/g)
-    .withMessage("As datas devem ser informadas no padrão yyyy-mm-dd, e separadas por ','"),
-  query("cnpj")
-    .optional()
-    .matches(/^\d{14}/g)
-    .withMessage("O CNPJ deve ser informado com 14 dígitos, sem pontos, traços ou barra"),
-  validate
-];
+export const validateCompanyQuery = celebrate({
+  [Segments.QUERY]: Joi.object({
+    cnpj: Joi.string()
+      .optional()
+      .length(14)
+      .message("O CNPJ deve ser informado com 14 dígitos.")
+      .pattern(/^\d{14}$/)
+      .message("O CNPJ deve ser informado sem pontos, traços ou barras."),
+    created_at: Joi.string()
+      .optional()
+      .pattern(/^(\d{4})-(\d{02})-(\d{02})\,\s?(\d{4})-(\d{2})-(\d{2})$/)
+      .message("As datas devem ser informadas no padrão yyyy-mm-dd, e separadas por ','"),
+    demand: Joi.string()
+      .optional()
+      .pattern(/^\d+\,\s?\d+$/)
+  }).options({ allowUnknown: true })
+});
