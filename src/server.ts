2dfea4ccd936456e51f67611ba5f84f8a0637a9a
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-05-04 22:03:53 -0300

    refactor: separate responsability

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,12 @@
 import "dotenv";
 import express from "express";
 import "./database";
-import { getCustomRepository } from "typeorm";
-import { AnnualBillingRepository } from "./repositories/AnnualBillingRepository";
-import { CompanyRepository } from "./repositories/CompanyRepository";
-import { CompanyService } from "./services/CompanyService";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", async (req, res) => {
-  const companyRepository = getCustomRepository(CompanyRepository);
-  const companies = await companyRepository.find({
-    relations: ["annual_billing"]
-  });
-  return res.json(companies);
-});
-
-app.get("/companies", async (req, res) => {
-  const companyService = new CompanyService();
-  const result = await companyService.findAll(req.query);
-  return res.json(result);
-});
+app.use(routes);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log("Server running on http://localhost:" + PORT));
