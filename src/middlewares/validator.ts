4e2557642f4878f8bf1024219b33fa58e1f14403
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-05-10 10:48:59 -0300

    feat: add validators to body and param

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -17,3 +17,33 @@ export const validateCompanyQuery = celebrate({
       .pattern(/^\d+\,\s?\d+$/)
   }).options({ allowUnknown: true })
 });
+
+export const validateCompanyBody = celebrate({
+  [Segments.BODY]: Joi.object({
+    name: Joi.string()
+      .required(),
+    cnpj: Joi.string()
+      .required()
+      .length(14)
+      .message("O CNPJ deve ser informado com 14 dígitos.")
+      .pattern(/^\d{14}$/)
+      .message("O CNPJ deve ser informado sem pontos, traços ou barras."),
+    demand: Joi.number()
+      .required(),
+    about: Joi.string()
+      .required(),
+    annual_billing_id: Joi.string()
+      .required()
+      .uuid({ separator: "-", version: "uuidv4" })
+      .message("O campo 'annual_billing_id' é do tipo UUID v4, e deve conter 36 caracteres.")
+  })
+});
+
+export const validateCompanyParams = celebrate({
+  [Segments.PARAMS]: Joi.object({
+    id: Joi.string()
+      .required()
+      .uuid({ separator: "-", version: "uuidv4" })
+      .message("O parametro 'id' é do tipo UUID v4, e deve conter 36 caracteres.")
+  })
+});
